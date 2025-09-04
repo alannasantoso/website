178081cc4d6e53b25ebb10586ae8e471cc5dd7c7
Alanna <alannasantoso22@gmail.com>
2025-09-05 11:15:23 +1200

    fix: header responsiveness

--- alannasantoso/src/components/Header.tsx
+++ alannasantoso/src/components/Header.tsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <header className="">
-      <nav className="text-grey-100 text-sm font-normal container mx-auto flex justify-center items-center p-7">
-        <div className="space-x-10">
+      <nav className="text-grey-100 text-sm font-normal container mx-auto flex flex-wrap pl-10 pr-10 justify-center items-center p-7">
+        <div className="space-x-10 flex flex-wrap justify-left items-center mx-auto">
           <Link to="/" className="hover:italic opacity-50 hover:opacity-100">&gt; Home</Link>
           <Link to="/about" className="hover:italic opacity-50 hover:opacity-100">&gt; About</Link>
           <Link to="/photos" className="hover:italic opacity-50 hover:opacity-100">&gt; Photos</Link>
